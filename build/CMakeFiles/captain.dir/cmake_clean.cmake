e9e553a4daa566dd30ae0a8213e21ddb07f9fedc
a-liukun <995340133@qq.com>
2023-07-19 20:20:39 +0800

    update

---
+++ build/CMakeFiles/captain.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../lib/libcaptain.pdb"
+  "../lib/libcaptain.so"
+  "CMakeFiles/captain.dir/captain/log.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/captain.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
