35ae6dc3e736f5efb3512db2a9ae3fe91f6eb24b
a-liukun <995340133@qq.com>
2023-07-26 21:10:13 +0800

    update

--- tests/test_thread.cpp
+++ tests/test_thread.cpp
@@ -3,25 +3,16 @@
 
 captain::Logger::ptr g_logger = CAPTAIN_LOG_ROOT();
 
-// int count = 0;
-
 void fun1() {
     CAPTAIN_LOG_INFO(g_logger) << "name: " << captain::Thread::GetName()
                              << " this.name: " << captain::Thread::GetThis()->getName()
                              << " id: " << captain::GetThreadId()
                              << " this.id: " << captain::Thread::GetThis()->getId();
 
-    // for(int i = 0; i < 100000; ++i) {
-    //     //captain::RWMutex::WriteLock lock(s_mutex);
-    //     //captain::Mutex::Lock lock(s_mutex);
-    //     ++count;
-    // }
 }
 
 void fun2() {
-    // while(true) {
-    //     CAPTAIN_LOG_INFO(g_logger) << "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
-    // }
+
 }
 
 int main(int argc, char** argv) {
@@ -30,16 +21,13 @@ int main(int argc, char** argv) {
     std::vector<captain::Thread::ptr> thrs;  //线程池
     for(int i = 0; i < 5; ++i) {
         captain::Thread::ptr thr(new captain::Thread(&fun1, "name_" + std::to_string(i)));
-        //captain::Thread::ptr thr2(new captain::Thread(&fun3, "name_" + std::to_string(i * 2 + 1)));
         thrs.push_back(thr);
-        //thrs.push_back(thr2);
     }
 
     for(size_t i = 0; i < thrs.size(); ++i) {
         thrs[i]->join();
     }
     CAPTAIN_LOG_INFO(g_logger) << "thread test end";
-    // CAPTAIN_LOG_INFO(g_logger) << "count=" << count;
 
     return 0;
 }
