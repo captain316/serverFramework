d3aef9f38c3834b9402c6c10f0eb9ce04cd62542
a-liukun <995340133@qq.com>
2023-08-28 08:49:57 +0800

    封装nocopy类,给thread继承

---
+++ captain/include/noncopyable.h
@@ -0,0 +1,16 @@
+#ifndef __CAPTAIN_NONCOPYABLE_H__
+#define __CAPTAIN_NONCOPYABLE_H__
+
+namespace captain {
+
+class Noncopyable {
+public:
+    Noncopyable() = default;
+    ~Noncopyable() = default;
+    Noncopyable(const Noncopyable&) = delete;
+    Noncopyable& operator=(const Noncopyable&) = delete;
+};
+
+}
+
+#endif
